f1c3484ab211c6319683bcab60f051b616a12e89
Pusat-bgl <pamfaiz64@gmail.com>
2026-08-11 14:17:36 +0700

    update google analys

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { DevToast } from "@/components/layout/DevToast";
 import { Toaster } from "@/components/ui/sonner";
@@ -44,6 +45,16 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col custom-scrollbar overflow-x-hidden">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-M3PL8V36K6" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-M3PL8V36K6');
+          `}
+        </Script>
         <UIProvider>
           {children}
           
