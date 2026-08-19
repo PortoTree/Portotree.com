178825e69f3df5a1ec38accd5d53f8366eaa656b
Pusat-bgl <pamfaiz64@gmail.com>
2026-08-19 20:16:59 +0700

    update 104 PRODUCTION ADS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,12 +47,11 @@ export default function RootLayout({
     >
       <head>
         {/* Google AdSense */}
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9324122345100415"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        ></script>
       </head>
       <body className="min-h-full flex flex-col custom-scrollbar overflow-x-hidden">
         <GoogleAnalytics gaId="G-M3PL8V36K6" />
